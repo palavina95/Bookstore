2bbbb4a1d8bab382f5f8a584864df09ffe31119d
palavina95 <37210893+palavina95@users.noreply.github.com>
2019-09-10 12:31:39 +0300

    First commit

---
+++ src/main/java/com/example/w2ex4BookStore/W2ex4BookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.w2ex4BookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W2ex4BookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W2ex4BookStoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/w2ex4BookStore/web/BookController.java
@@ -0,0 +1,14 @@
+package com.example.w2ex4BookStore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+	@RequestMapping("/index")
+	public String greeting(Model model) {
+		return "";
+		
+	}
+}
