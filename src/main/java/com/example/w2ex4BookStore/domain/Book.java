ac47ea81da3bfdfc00a99d62f399bdac690f2b81
palavina95 <vivian.bridy@students.hevs.ch>
2019-09-14 14:18:35 +0300

    RESTful OK Chap5

--- src/main/java/com/example/w2ex4BookStore/domain/Book.java
+++ src/main/java/com/example/w2ex4BookStore/domain/Book.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Book {
@@ -18,6 +20,7 @@ public class Book {
 	private double price;
 	
 	@ManyToOne
+	@JsonIgnore
     @JoinColumn(name = "categoryId")
     private Category category;
 	
--- src/main/java/com/example/w2ex4BookStore/web/BookController.java
+++ src/main/java/com/example/w2ex4BookStore/web/BookController.java
@@ -1,15 +1,20 @@
 package com.example.w2ex4BookStore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.w2ex4BookStore.domain.Book;
 import com.example.w2ex4BookStore.domain.BookRepository;
 import com.example.w2ex4BookStore.domain.CategoryRepository;
+
 
 //1st Comment 
 
@@ -22,12 +27,26 @@ public class BookController {
 	@Autowired
 	private CategoryRepository catRepository;
 	
+	//Show all books
 	@RequestMapping("/index")
 	public String greeting(Model model) {
 		model.addAttribute("booklist",repository.findAll());
 		return "booklist";
 	}
 	
+	// RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {	
+        return (List<Book>) repository.findAll();
+    } 
+    
+    // RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+    public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {	
+    	return repository.findById(bookId);
+    } 
+	
+    //Add new book
 	@RequestMapping(value = "/add")
     public String addStudent(Model model){
     	model.addAttribute("book", new Book());
@@ -35,21 +54,25 @@ public class BookController {
         return "addBook";
     }
 	
+	//Save new book
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
     public String save(Book book){
         repository.save(book);
         return "redirect:index";
     }
 	
+	//Delete book
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
         return "redirect:../index";
     } 
 	
+	//Edit a book
 	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
     public String editBook(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
+		model.addAttribute("categories", catRepository.findAll());
         return "editBook";
     }
 	
