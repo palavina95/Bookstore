7125d901f4681d469bb0e6eba7169577bf525bd1
palavina95 <vivian.bridy@students.hevs.ch>
2019-09-14 15:33:01 +0300

    Securing the web application OK part2

--- src/main/java/com/example/w2ex4BookStore/BookStoreApplication.java
+++ src/main/java/com/example/w2ex4BookStore/BookStoreApplication.java
@@ -11,7 +11,8 @@ import com.example.w2ex4BookStore.domain.Book;
 import com.example.w2ex4BookStore.domain.BookRepository;
 import com.example.w2ex4BookStore.domain.Category;
 import com.example.w2ex4BookStore.domain.CategoryRepository;
-
+import com.example.w2ex4BookStore.domain.User;
+import com.example.w2ex4BookStore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -22,14 +23,21 @@ public class BookStoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookInit(BookRepository repository, CategoryRepository catRepository) {
+	public CommandLineRunner bookInit(BookRepository repository, CategoryRepository catRepository, UserRepository uRepository) {
 		return (args) -> {
 			log.info("save a couple of categories");
 			catRepository.save(new Category("Tragedy"));
 			catRepository.save(new Category("New"));
+			
 			log.info("save a couple of books");
 			repository.save(new Book("Martin1","Rowlings","2005","PQL-156",14.9,catRepository.findByName("Tragedy").get(0)));
 			repository.save(new Book("Martin2","Rowlings","2006","PQL-157",12.9,catRepository.findByName("New").get(0)));
+			
+			// Create users: admin/admin user/user
+			User user1 = new User("user", "$2a$10$pcyvJXKC3KyOBx988OOBB.Zx6qTNCWJoUNWW7rAJQPpOWPz0mo/n2", "USER");
+			User user2 = new User("admin", "$2a$10$qzjN5i5Azvoq8abOZRbVeu63FKFG3uKKjYLNlu7VM5NFWrW1oIAjK", "ADMIN");
+			uRepository.save(user1);
+			uRepository.save(user2);
 			
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
--- src/main/java/com/example/w2ex4BookStore/WebSecurityConfig.java
+++ src/main/java/com/example/w2ex4BookStore/WebSecurityConfig.java
@@ -3,20 +3,29 @@ package com.example.w2ex4BookStore;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import com.example.w2ex4BookStore.web.UserDetailServiceImpl;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+    private UserDetailServiceImpl userDetailsService;	
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -57,6 +66,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	users.add(user);
     	
         return new InMemoryUserDetailsManager(users);
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
 }
